63320f968e39a6f9bbdf9018886c25cd31d74c24
Infinite Loop <teardown@inbox.ru>
2021-11-25 23:27:43 +0300

    Updated MIDI device selection dialog to use drop-down list instead of several buttons MIDI device interface implementation refactoring

--- lua/midi_interface/base.lua
+++ lua/midi_interface/base.lua
@@ -35,11 +35,40 @@ end
 
 -- Print functions that prefix "MIDI" with colour
 function piano_midi.print(...)
-    MsgC(Color( 0, 255, 255), "Piano MIDI: ", Color(220, 220, 220), printPre(true, ...))
+    MsgC(Color(0, 255, 255), "Piano MIDI: ", Color(220, 220, 220), printPre(true, ...))
 end
 
 function piano_midi.printChat(...)
     chat.AddText(Color(0, 255, 255), "Piano MIDI: ", Color(220, 220, 220), printPre(false, ...))
+end
+
+function piano_midi.eventHook(time, command, note, velocity, ...)
+    if not command then return end
+    local code = midi.GetCommandCode(command)
+    local name = midi.GetCommandName(command)
+    -- Zero velocity NOTE_ON substitutes NOTE_OFF
+    if name == "NOTE_ON" and velocity == 0 then
+        name = "NOTE_OFF"
+    end
+    -- Do debug print if enabled
+    local cVar = GetConVar("midi_debug")
+    if cVar and cVar:GetBool() then
+        -- The code is a byte (number between 0 and 254).
+        piano_midi.print(" = == EVENT = = =")
+        piano_midi.print("Time:\t", time)
+        piano_midi.print("Code:\t", code)
+        piano_midi.print("Channel:\t", midi.GetCommandChannel(command))
+        piano_midi.print("Name:\t", name)
+        piano_midi.print("Parameters", note, velocity, ...)
+    end
+    -- Get instrument entity
+    local instrument = LocalPlayer().Instrument
+    if midi and IsValid(instrument) then
+        if (name == "NOTE_ON" or name == "NOTE_OFF") and
+           (note > 35) and (note <= (35 + instrument.SemitonesNum)) then
+            piano_midi.sendNote(instrument, note - 35, velocity)
+        end
+    end
 end
 
 function piano_midi.load()
@@ -49,58 +78,16 @@ function piano_midi.load()
        file.Exists("lua/bin/gmcl_midi_win64.dll", "MOD") or
        file.Exists("lua/bin/gmcl_midi_linux.dll", "MOD") or
        file.Exists("lua/bin/gmcl_midi_linux64.dll", "MOD") then
-
         piano_midi.print("GMCL MIDI module detected!")
         require("midi") -- Import the library
-
-        if not midi then -- Check it succeeded
+        if midi then -- Check it succeeded
+            piano_midi.printChat("GMCL MIDI module initialised. Use console commands midi_devices and midi_debug [0|1] to use.")
+            hook.Add("MIDI", "midiPlayablePiano", piano_midi.eventHook)
+            -- Tell others it worked
+            hook.Run("piano_midi_init", midi)
+        else
             print("Failed to initialise GMCL MIDI module.")
-            return
         end
-        piano_midi.printChat("GMCL MIDI module initialised. Use console commands midi_devices and midi_debug [0|1] to use.")
-
-        -- Connect to first device if it exists for convenience
-        local ports = midi.GetPorts()
-        local portsCount = table.Count(ports)
-        if portsCount > 0 then
-            midi.Open(0)
-            piano_midi.print("Connected to device " .. ports[0])
-        end
-
-        hook.Add("MIDI", "midiPlayablePiano", function(time, command, note, velocity, ...)
-            if not command then return end
-
-            local code = midi.GetCommandCode(command)
-            local name = midi.GetCommandName(command)
-            -- Zero velocity NOTE_ON substitutes NOTE_OFF
-            if name == "NOTE_ON" and velocity == 0 then
-                name = "NOTE_OFF"
-            end
-
-            -- Do debug print if enabled
-            local cVar = GetConVar("midi_debug")
-            if cVar and cVar:GetBool() then
-                -- The code is a byte ( number between 0 and 254 ).
-                piano_midi.print(" = == EVENT = = =")
-                piano_midi.print("Time:\t", time)
-                piano_midi.print("Code:\t", code)
-                piano_midi.print("Channel:\t", midi.GetCommandChannel(command))
-                piano_midi.print("Name:\t", name)
-                piano_midi.print("Parameters", note, velocity, ...)
-            end
-
-            -- Get instrument entity
-            local instrument = LocalPlayer().Instrument
-            if not IsValid(instrument) then return end
-
-            if midi and (name == "NOTE_ON" or name == "NOTE_OFF") and
-               (note > 35) and (note <= (35 + instrument.SemitonesNum)) then
-                piano_midi.sendNote(instrument, note - 35, velocity)
-            end
-        end)
-
-        -- Tell others it worked
-        hook.Run("piano_midi_init", midi)
     end
 end
 
--- lua/midi_interface/console.lua
+++ lua/midi_interface/console.lua
@@ -1,5 +1,83 @@
+local MI_INVALID_DEVICE_NAME = "None"
 local concommand = concommand
+local deviceInUse = MI_INVALID_DEVICE_NAME
 local midi
+
+function Derma_OptionRequest(strTitle, strText, options, selectedIndex,
+                             fnEnter, fnCancel,
+                             strButtonText, strButtonCancelText)
+	local Window = vgui.Create("DFrame")
+	Window:SetTitle(strTitle or "Message Title (First Parameter)")
+	Window:SetDraggable(false)
+	Window:ShowCloseButton(false)
+	Window:SetBackgroundBlur(true)
+	Window:SetDrawOnTop(true)
+
+	local InnerPanel = vgui.Create("DPanel", Window)
+	InnerPanel:SetPaintBackground(false)
+
+	local Text = vgui.Create("DLabel", InnerPanel)
+	Text:SetText(strText or "Message Text (Second Parameter)")
+	Text:SizeToContents()
+	Text:SetContentAlignment(5)
+	Text:SetTextColor(color_white)
+
+	local ComboBox = vgui.Create("DComboBox", InnerPanel)
+    for index, optionText in pairs(options) do
+        ComboBox:AddChoice(optionText)
+    end
+	ComboBox:ChooseOptionID(selectedIndex or 1)
+	ComboBox.OnEnter = function()
+        Window:Close()
+        fnEnter(ComboBox:GetSelectedID())
+    end
+
+	local ButtonPanel = vgui.Create("DPanel", Window)
+	ButtonPanel:SetTall(30)
+	ButtonPanel:SetPaintBackground(false)
+
+	local Button = vgui.Create("DButton", ButtonPanel)
+	Button:SetText(strButtonText or "OK")
+	Button:SizeToContents()
+	Button:SetTall(20)
+	Button:SetWide(Button:GetWide() + 20)
+	Button:SetPos(5, 5)
+	Button.DoClick = function()
+        Window:Close()
+        fnEnter(ComboBox:GetSelectedID())
+    end
+
+	local ButtonCancel = vgui.Create("DButton", ButtonPanel)
+	ButtonCancel:SetText(strButtonCancelText or "Cancel")
+	ButtonCancel:SizeToContents()
+	ButtonCancel:SetTall(20)
+	ButtonCancel:SetWide(Button:GetWide() + 20)
+	ButtonCancel:SetPos(5, 5)
+	ButtonCancel:MoveRightOf(Button, 5)
+	ButtonCancel.DoClick = function()
+        Window:Close()
+        if (fnCancel) then
+            fnCancel(ComboBox:GetSelectedID())
+        end
+    end
+
+	local w, h = Text:GetSize()
+	w = math.max(w, 400)
+	Window:SetSize(w + 50, h + 25 + 75 + 10)
+	Window:Center()
+	InnerPanel:StretchToParent(5, 25, 5, 45)
+	Text:StretchToParent(5, 5, 5, 35)
+	ComboBox:StretchToParent(5, nil, 5, nil)
+	ComboBox:AlignBottom(5)
+	ComboBox:RequestFocus()
+	ComboBox:SelectAllText(true)
+	ButtonPanel:SetWide(Button:GetWide() + 5 + ButtonCancel:GetWide() + 10)
+	ButtonPanel:CenterHorizontal()
+	ButtonPanel:AlignBottom(8)
+	Window:MakePopup()
+	Window:DoModal()
+	return Window
+end
 
 local function midiClose()
     if midi.IsOpened() then
@@ -8,43 +86,60 @@ local function midiClose()
     end
 end
 
--- Generate callbacks for Derma_Query
-local function midiOption(ports, i)
-    return function()
-        if ports[i] then
-            midiClose()
-            midi.Open(i)
-            piano_midi.print("Connected to device " .. ports[i])
-        end
+local function midiOpen(ports, index)
+    if ports[index] then
+        midiClose()
+        midi.Open(index)
+        piano_midi.print("Connected to device " .. ports[index])
     end
 end
 
-concommand.Add("midi_reload", function(ply, cmd, args)
+local function midiReload(ply, cmd, args)
     piano_midi.print("Reloading...")
     piano_midi.load()
-end)
+end
 
-hook.Add("piano_midi_init", "piano_midi_console", function(_midi)
-    midi = _midi
-
-    -- Pop up interface to let you select a midi device
-    concommand.Add("midi_devices", function(ply, cmd, args)
-        local ports = midi.GetPorts()
-        local portsCount = table.Count(ports)
-
-        if portsCount > 0 then
-            piano_midi.print("Opening menu...")
-            Derma_Query("Which device you would like to use?" .. (portsCount > 3 and " (Max. 3 devices)" or ""),
-                "Device selection",
-                "Disable", midiClose,
-                ports[0], midiOption(ports, 0),
-                ports[1], midiOption(ports, 1),
-                ports[2], midiOption(ports, 2)
-            )
-        else
-            piano_midi.print("No devices connected.")
+local function midiDevices(ply, cmd, args)
+    local ports = midi.GetPorts()
+    local portsCount = table.Count(ports)
+    if portsCount > 0 then
+        piano_midi.print("Opening menu...")
+        local devices = { MI_INVALID_DEVICE_NAME }
+        for index, deviceName in pairs(ports) do
+            devices[index + 2] = deviceName
         end
-    end)
-    CreateConVar("midi_debug", "0", FCVAR_ARCHIVE, "Should MIDI events be printed to chat", 0, 1)
-end)
+        local deviceIndex = table.KeyFromValue(devices, deviceInUse)
+        if not deviceIndex then
+            deviceIndex = 1
+        end
+        Derma_OptionRequest("Device selection", "Which device you would like to use?",
+                            devices, deviceIndex, function(index)
+                                deviceInUse = devices[index]
+                                if index > 1 then
+                                    midiOpen(ports, index - 2)
+                                else
+                                    midiClose()
+                                end
+                            end)
+    else
+        piano_midi.print("No devices connected.")
+    end
+end
 
+local function midiInit(_midi)
+    deviceInUse = MI_INVALID_DEVICE_NAME
+    midi = _midi
+    -- Connect to first device if it exists for convenience
+    local ports = midi.GetPorts()
+    local portsCount = table.Count(ports)
+    if portsCount > 0 then
+        deviceInUse = ports[0]
+        midiOpen(ports, 0)
+    end
+    -- Pop up interface to let you select a midi device
+    concommand.Add("midi_devices", midiDevices)
+    CreateConVar("midi_debug", "0", FCVAR_ARCHIVE, "Should MIDI events be printed to chat", 0, 1)
+end
+
+concommand.Add("midi_reload", midiReload)
+hook.Add("piano_midi_init", "piano_midi_console", midiInit)
